fe9620177947bd6da61fada5f8d2fa2bbd20f629
Dawon <jcn07253@gmail.com>
2023-09-14 21:39:35 +0900

    Revert "실습"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-// src/App.js
 import React from 'react';
 
 function App() {
-  const items = ["Item 1", "Item 2", "Item 3"];
-
   return (
     <div className="App">
-      <h1>Simple List Example</h1>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <header className="App-header">
+        <h1>Hello, World!</h1>
+      </header>
     </div>
   );
 }
